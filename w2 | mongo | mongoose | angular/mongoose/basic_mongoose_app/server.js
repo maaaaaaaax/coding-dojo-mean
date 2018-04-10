c74f74fccf8cdf715fc3af3e8f29c0472225f107
Max Wiederholt <max.wiederholt@gmail.com>
2018-04-10 12:32:32 -0700

    finished quoting_dojo

---
+++ w2 | mongo | mongoose | angular/mongoose/basic_mongoose_app/server.js
@@ -0,0 +1,70 @@
+// require express, body-parser, express-session
+var express = require("express");
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var path = require("path");
+var mongoose = require('mongoose');
+// This is how we connect to the mongodb database using mongoose -- "basic_mongoose" is the name of
+//   our db in mongodb -- this should match the name of the db you are going to use for your project.
+mongoose.connect('mongodb://localhost/basic_mongoose');
+var UserSchema = new mongoose.Schema({
+ name: String,
+ age: Number
+})
+mongoose.model('User', UserSchema); // We are setting this Schema in our Models as 'User'
+var User = mongoose.model('User') // We are retrieving this Schema from our Models, named 'User'
+
+// create the express app
+var app = express();
+app.use(session({secret: 'shhhhhh'}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// setting up static content folder
+app.use(express.static(path.join(__dirname, "./static")));
+// setting up ejs and our views folder
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+// ROUTES
+
+// The root route -- we want to get all of the users from the database and then render the index view passing it all of the users
+app.get('/', function(req, res) {
+  User.find({}, function(err, users) {
+    // This is the method that finds all of the users from the database
+    // Notice how the first parameter is the options for what to find and the second is the
+    //   callback function that has an error (if any) and all of the users
+    // Keep in mind that everything you want to do AFTER you get the users from the database must
+    //   happen inside of this callback for it to be synchronous
+    // Make sure you handle the case when there is an error, as well as the case when there is no error
+    // if there is an error console.log that something went wrong!
+    if(err) {
+      console.log('something went wrong');
+    } else { // else console.log that we did well and then redirect to the root route
+      console.log('successfully found all users!');
+      console.log(users);
+      res.render('index', {users: users});
+    }
+  })
+})
+
+// Add User Request
+app.post('/users', function(req, res) {
+  console.log("POST DATA", req.body);
+  // create a new User with the name and age corresponding to those from req.body
+  var user = new User({name: req.body.name, age: req.body.age});
+  // Try to save that new user to the database (this is the method that actually inserts into the db) and run a callback function with an error (if any) from the operation.
+  user.save(function(err) {
+    // if there is an error console.log that something went wrong!
+    if(err) {
+      console.log('something went wrong');
+    } else { // else console.log that we did well and then redirect to the root route
+      console.log('successfully added a user!');
+      res.redirect('/');
+    }
+  })
+})
+
+// Setting our Server to Listen on Port: 8000
+app.listen(8000, function() {
+    console.log("listening on port 8000");
+})
